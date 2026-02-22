3944ee442fec19e7820f292774abb480e1091390
iriduan900 <iriduan900@gmail.com>
2026-02-22 22:42:55 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // 🔒 Configure your portal here
 window.LOCKEDTUBE_CONFIG = {
   // Required: YouTube Data API v3 key
-  apiKey: "AIzaSyCBfA_anTR0ZolneATQnH1NkP2Nmj9fw8g",
+  apiKey: AIzaSyCBfA_anTR0ZolneATQnH1NkP2Nmj9fw8g,
 
   // Allowed channel handles (without @)
   allowedHandles: [
-    "MrBeast"
+    MrBeast
     // "AnotherHandle"
   ],
 
