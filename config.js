70a6b2a4f4bb6b8205781fbb47917d42853948fd
iriduan900 <iriduan900@gmail.com>
2026-02-22 23:00:03 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
-// 🔒 Configure your portal here
 window.LOCKEDTUBE_CONFIG = {
-  // ⚠️ You should restrict/rotate this key in Google Cloud Console (HTTP referrers).
   apiKey: "AIzaSyCBfA_anTR0ZolneATQnH1NkP2Nmj9fw8g",
-
-  // Allowed channel handles (without @)
   allowedHandles: ["MrBeast"],
-
-  // Behavior
   maxPerChannel: 24,
   hideShorts: true
 };
